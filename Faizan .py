f5a868fad017642786837143c6cbf72f18d99979
MalaikaAyub0011 <bsmth22f23@namal.edu.pk>
2024-09-12 14:36:02 +0500

    Add files via upload

---
+++ Faizan .py
@@ -0,0 +1,18 @@
+
+
+num1=eval(input("enter a first number"))
+
+num2=eval(input("enter a second number"))
+operator=input("use operator (+,-,/,*)")
+if operator=="+":
+   result=num1+num2
+   print(result)
+elif operator=="-":
+     result=num1-num2
+     print(result)
+elif operator=="/":
+     result=num1/num2
+     print(result)
+elif operator=="*":
+     result=num1*num2
+     print(result)
